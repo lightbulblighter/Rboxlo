8f6e4708f021714f18a82d0a5e7e192957c3532c
lighterlightbulb <59720715+lighterlightbulb@users.noreply.github.com>
2020-04-14 13:52:32 -0500

    Add Rboxlo SQL database structure dump

---
+++ RboxloWebsite/rboxlo.sql
@@ -0,0 +1,96 @@
+-- Adminer 4.7.6 MySQL dump
+
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+DROP TABLE IF EXISTS `api_keys`;
+CREATE TABLE `api_keys` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `version` text NOT NULL,
+  `usage` text NOT NULL,
+  `key` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+DROP TABLE IF EXISTS `client_hashes`;
+CREATE TABLE `client_hashes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `version` text NOT NULL,
+  `hash` text NOT NULL,
+  `type` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+DROP TABLE IF EXISTS `client_versions`;
+CREATE TABLE `client_versions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `version` text NOT NULL,
+  `details` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+DROP TABLE IF EXISTS `email_verification_keys`;
+CREATE TABLE `email_verification_keys` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `key` text NOT NULL,
+  `uid` int(11) NOT NULL,
+  `used` int(11) NOT NULL DEFAULT '0',
+  `generated` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+SET NAMES utf8mb4;
+
+DROP TABLE IF EXISTS `friends`;
+CREATE TABLE `friends` (
+  `id` int(11) NOT NULL,
+  `sender_uid` int(11) NOT NULL,
+  `recipient_uid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+DROP TABLE IF EXISTS `invite_keys`;
+CREATE TABLE `invite_keys` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uses` int(11) NOT NULL,
+  `max_uses` int(11) NOT NULL,
+  `key` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+DROP TABLE IF EXISTS `news`;
+CREATE TABLE `news` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `color` tinytext NOT NULL,
+  `message` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` text NOT NULL,
+  `password` text NOT NULL,
+  `email` text NOT NULL,
+  `register_ip` text NOT NULL,
+  `last_ip` text NOT NULL,
+  `coins` bigint(20) NOT NULL DEFAULT '200',
+  `joindate` int(11) NOT NULL,
+  `avatar` text NOT NULL,
+  `email_verified` tinyint(4) NOT NULL DEFAULT '0',
+  `preferences` text NOT NULL,
+  `rank` tinyint(4) NOT NULL DEFAULT '0',
+  `ssc` tinyint(4) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+-- 2020-04-14 18:51:48
