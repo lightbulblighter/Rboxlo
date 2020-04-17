76486827f78c953794e4856555cccd3e1b88786d
lighterlightbulb <59720715+lighterlightbulb@users.noreply.github.com>
2020-04-17 00:36:06 -0500

    Fixed whitespace in about page

--- RboxloWebsite/html/about/mission.php
+++ RboxloWebsite/html/about/mission.php
@@ -31,11 +31,11 @@
 				<div class="rounded-top mdb-color purple accent-3 pt-3 pl-3 pb-3">
 					<span class="white-text">What is this?</span>
 				</div>
-				<div class="card-body mx-4">
+				<div class="card-body">
 					<p>
 						<?php echo(BASE_NAME); ?> is a 100% transparent, 100% open source Roblox private server. We want to recreate the fun of playing Roblox from all client versions 2007 through 2017
 						without imposing restrictions on developers, users, or anybody who wants to play <?php echo(BASE_NAME); ?>.
-						<br>
+						<br><br>
 						We accomplish this in several ways:
 						<ul>
 							<li>No profanity filter</li>
@@ -44,7 +44,7 @@
 							<li>Being open source so people can contribute, fix, solve issues, and develop for <?php echo(BASE_NAME); ?></li>
 							<li>Maintaining 100% transparency in what staff members do (see staff audits <a href="/transparency/">here</a>)</li>
 						</ul>
-						<br>
+						
 						<?php echo(BASE_NAME); ?> is created with the sense of transparency at it's heart, after many other Roblox private servers did not uphold transparency fully.
 					</p>
 				</div>  
@@ -59,10 +59,10 @@
 				<div class="card-body">
 					<p>
 						Our goal is to be as transparent and open source as possible and not creating any doubt in newcomers or users of <?php echo(BASE_NAME); ?>.
-						<br>
+						<br><br>
 						Most Roblox private servers opt to be closed-source, and very opaque so they can admin abuse and treat the community very poorly; and, as we have seen with those private servers, the community manifests
 						into a savage beast that requires a huge staff team (sometimes more than 20 people!) to tame without getting hurt.
-						<br>
+						<br><br>
 						We would also like to teach users how to program using PHP, JavaScript, ASM, or other languages by open sourcing all the works that we have created.
 					</p>
 				</div>  
@@ -77,7 +77,7 @@
 				<div class="card-body">
 					<p>
 						<?php echo(BASE_NAME); ?> is created by a team of young, aspiring developers of whose contributions you can see on the <a href="<?php echo(GITHUB_URL); ?>/graphs/contributors">GitHub contributors page.</a>
-						<br>
+						<br><br>
 						You can contribute too! Create a pull request, solve an issue, or add a new feature; these are the beauties of being open source.
 					</p>
 				</div>  
