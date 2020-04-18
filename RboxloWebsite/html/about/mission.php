31c331d826a5a4c22021ebad685aecdfadf6f358
lighterlightbulb <59720715+lighterlightbulb@users.noreply.github.com>
2020-04-17 21:33:28 -0500

    Make mission page more responsive

--- RboxloWebsite/html/about/mission.php
+++ RboxloWebsite/html/about/mission.php
@@ -19,7 +19,7 @@
         <div class="jumbotron card card-image" style="background-image: url(/core/html/img/about_backdrop.png)">
             <div class="text-white text-center">
                 <div>
-					<img src="/core/html/img/full.png" class="img-fluid" style="max-width: 600px">
+					<img src="/core/html/img/full.png" class="img-fluid" style="width: 600px">
 					<br>
 					<h1 class="card-title h1-responsive">About Us</h1>
                 </div>
