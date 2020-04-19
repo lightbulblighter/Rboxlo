6350605172712bdf747dc8789e01d2d175288a64
lighterlightbulb <59720715+lighterlightbulb@users.noreply.github.com>
2020-04-19 15:21:05 -0500

    Round CPU usage

--- website/html/endpoints/statistics.php
+++ website/html/endpoints/statistics.php
@@ -3,7 +3,7 @@
 
     exit(json_encode([
         "time" => time(),
-        "cpu" => get_server_cpu_usage(),
+        "cpu" => round(get_server_cpu_usage()),
         "ram" => round(get_server_memory_usage())
     ]))
 ?>
