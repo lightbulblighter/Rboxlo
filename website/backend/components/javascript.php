08feae654846b2cba65c6acea48ec73f4afe5b48
lighterlightbulb <59720715+lighterlightbulb@users.noreply.github.com>
2020-05-24 22:14:19 -0500

    Bump jQuery version

--- website/backend/components/javascript.php
+++ website/backend/components/javascript.php
@@ -1,5 +1,5 @@
 <!-- JAVASCRIPT -->
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.0/jquery.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.3.3/umd/popper.min.js"></script>
 <script type="text/javascript" src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 <script type="text/javascript" src="<?php echo(get_server_host()); ?>/html/js/mdb.min.js"></script>
