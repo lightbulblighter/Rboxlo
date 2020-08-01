73e0b8ff5f474d8d7543db47a268d31b9adec78b
lightbulblighter <59720715+lighterlightbulb@users.noreply.github.com>
2020-08-01 02:16:36 -0500

    Documentation
    lo

--- website/data/environment/project.environment.php
+++ website/data/environment/project.environment.php
@@ -4,9 +4,14 @@
 
         PROJECT["NAME"] => The project's name.
         PROJECT["CURRENCY"] => The project's currency's name.
-        PROJECT["REFERRAL"] => Whether to not the project is on "invite-only" mode.
+        PROJECT["DISCORD"] => An invite to the project's Discord.
         PROJECT["DEBUGGING"] => Whether to show error messages or not, regardless of user identity.
+        PROJECT["COMMUNISM"] => Everything in the catalog is free.
         PROJECT["VALID_EMAIL_DOMAINS"] => An array containing the valid E-Mail domains that a user can sign up with (for example, with john@gmail.com, @gmail.com is the E-Mail domain)
+        PROJECT["PRIVATE"]["LOCKDOWN"] => Complete lockdown of site if true
+        PROJECT["PRIVATE"]["IMPLICATION"] => If false, removes register links, etc.
+        PROJECT["PRIVATE"]["REFERRAL"] => Referral-based private revival: You need an invite key from a friend. Like Finobe
+        PROJECT["PRIVATE"]["INVITE_ONLY"] => Invite-only-based private revival: You need a direct invite from an admin through a link
     */
     
     define("PROJECT", [
