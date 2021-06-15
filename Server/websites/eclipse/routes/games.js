e791d665270dc757b23c360f107b8bf43920dcfe
lightbulblighter <59720715+lightbulblighter@users.noreply.github.com>
2021-06-14 23:33:39 -0500

    [feat]: Game creation layouts, palette tweaks

--- Server/websites/eclipse/routes/games.js
+++ Server/websites/eclipse/routes/games.js
@@ -5,7 +5,15 @@ const path = require("path")
 const user = require(path.join(global.rboxlo.root, "websites", "eclipse", "lib", "user"))
 
 router.get("/", user.authenticated, (req, res) => {
-    res.render("games/index", { title: "Games" })
+    res.render("games/index", { title: "Games", objects: { canCreateGames: req.session.rboxlo.user.permissions.places.creation } })
+})
+
+router.get("/new", user.authenticated, (req, res) => {
+    if (!req.session.rboxlo.user.permissions.places.creation) {
+        return res.send(403)
+    }
+
+    res.render("games/new", { title: "New Game"})
 })
 
 module.exports = router
