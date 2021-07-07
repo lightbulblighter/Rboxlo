d4c8944b2e3bc6f96f4f35f0209543fafbc96401
lightbulblighter <59720715+lightbulblighter@users.noreply.github.com>
2021-07-07 03:09:07 -0500

    [minor]: Remove X-Powered-By in Tootsie middleware

--- Server/websites/tootsie/middleware.js
+++ Server/websites/tootsie/middleware.js
@@ -13,23 +13,6 @@ async function middleware(req, res) {
     
     req.session.rboxlo.bust = moment().unix() // resets contents each page req
 
-    //
-    // X-Powered-By header
-    // The string "Rboxlo" here is hardcoded in the form of a ASCII charcode array. Why?
-    //
-    // 1. Given the audience of Rboxlo, most edits scrubbing Rboxlo off of their private server will be through
-    //    complete search and replaces, rather than simply editing the environment file though that achieves the
-    //    same effect. Hardcoding it as an ASCII charcode array will hopefully deter that.
-    //
-    // 2. Rboxlo is an application that powers private servers. It is not a private server in itself. It is important
-    //    to retain the fact that it powers private servers.
-    //
-    if (global.rboxlo.env.SERVER_X_POWERED_BY) {
-        let poweredBy = [ 82, 98, 111, 120, 108, 111 ] // Literal "Rboxlo"
-        
-        res.setHeader("X-Powered-By", `${String.fromCharCode.apply(null, poweredBy)}/${util.getVersion().semver}`)
-    }
-
     // Get IP and store in req.rboxlo.ip
     let realip = req.connection.remoteAddress.trim()
     if (realip.startsWith("::ffff:")) realip = realip.slice(7)
